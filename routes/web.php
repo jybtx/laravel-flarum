7dff08764bb47fd9fce59990c13334d38b638efc
jybtx <2574034626@qq.com>
2020-06-10 13:35:47 +0800

    基础页面结构

--- routes/web.php
+++ routes/web.php
@@ -17,8 +17,6 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+// Route::get('/home', 'HomeController@index')->name('home');
 
-Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/', 'PagesController@root')->name('root');
